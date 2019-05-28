fdc9277a9860875776ad87bbb76006eaff1a0748
AadilMehdi Sanchawala <aadilmehdi.s@students.iiit.ac.in>
2019-05-28 13:55:31 +0530

    Changed normalization to calculate the mean and euclidean distance for the matched points rather than the entire image

--- getNormalizationTransformMtx.m
+++ getNormalizationTransformMtx.m
@@ -1,20 +1,16 @@
-function [T] = getNormalizationTransformMtx(I)    
-    % I: image for which the normalization/conditioning transform is to be
-    % calculated
+function [T] = getNormalizationTransformMtx(X)    
+    % X: coordinates of the matched points
     
     % T: normalization transform matrix
     
-    % Calculating the mean of the image
-    y = 1:size(I, 1);
-    x = 1:size(I, 2);
-    mu = [mean(x), mean(y)];
+    % Calculating the mean along the x and y coordinates for the given
+    % points
+    mu = [mean(X(:,1)), mean(X(:,2))];
     
-    
-    % Calculating the average distance of all the image points from the
-    % center of the image
-    imageCenter = size(I)./2;
-    distance = sqrt( (y.' - imageCenter(1)).^2 + (x - imageCenter(2)).^2 );
-    d = mean2(distance);
+    % Calculating the distance from the center of the of the image for the
+    % given points
+    distance = sqrt( (X(:,1) - mu(1)).^2 + (X(:,2) - mu(2)).^2 );
+    d = mean(distance);
     
     % Constructing T
     % Transforming the points so that the center of mass of the points lie
@@ -22,8 +18,8 @@ function [T] = getNormalizationTransformMtx(I)
     % Scale the image so the the coordinates of any point lie in the range
     % [-1 1]
 
-    T = [ 1.44/d,      0, -1.44/d * mu(1);
-               0, 1.44/d, -1.44/d * mu(2);
-               0,      0,               1;
+    T = [    1.44/d,     0,    -1.44/d * mu(1);
+               0,   1.44/d,    -1.44/d * mu(2);
+               0,        0,                  1;
          ];
 end
