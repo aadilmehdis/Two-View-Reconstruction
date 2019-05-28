4889e827393c6f5a73d6179f40456a50109d8edc
AadilMehdi Sanchawala <aadilmehdi.s@students.iiit.ac.in>
2019-05-28 11:09:56 +0530

    Cleaning up

--- getNormalizationTransformMtx.m
+++ getNormalizationTransformMtx.m
@@ -1,4 +1,4 @@
-function [T] = getNormalizationTransformMtx(I)
+function [T] = getNormalizationTransformMtx(I)    
     % I: image for which the normalization/conditioning transform is to be
     % calculated
     
@@ -17,20 +17,13 @@ function [T] = getNormalizationTransformMtx(I)
     d = mean2(distance);
     
     % Constructing T
+    % Transforming the points so that the center of mass of the points lie
+    % on (0, 0)
+    % Scale the image so the the coordinates of any point lie in the range
+    % [-1 1]
 
     T = [ 1.44/d,      0, -1.44/d * mu(1);
                0, 1.44/d, -1.44/d * mu(2);
                0,      0,               1;
          ];
-    
-    % Normalizing the Point's Cooridinates to obtain a Well Condition svd
-    % and improve the stability
-    
-    % Transforming the points so that the center of mass of the points lie
-    % on (0, 0)
-    
-    
-    
-    % Scale the image so the the coordinates of any point lie in the range
-    % [-1 1]
 end
